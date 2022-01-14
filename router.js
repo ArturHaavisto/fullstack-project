151c08e9218a4671f01309a6cda8accddead2cdc
ArturHaavisto <arturhaavisto1@gmail.com>
2022-01-14 13:45:25 +0200

    Added comments to router.js

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const connection = require("./crudrepository");
 const Validator = require("jsonschema").Validator;
 const validator = new Validator();
 
+/* Schema for english and finnish words. */
 const postSchema = {
   type: "string",
   pattern: "^[a-zäöå\\s]*$",
@@ -11,6 +12,13 @@ const postSchema = {
   maxLength: 100,
 };
 
+/**
+ * Validates the given word pair object.
+ * 
+ * @param {*} words - Data to be validated ({english: english word, finnish: finnish word}).
+ * @returns Returns either trimmed, lower cased and validated words back in the same way that recieved or
+ * returns errror messages as {msg: "errors", english: english error message, finnish: finnish error message}.
+ */
 function validatePost(words) {
   let english = words.english.trim().toLowerCase();
   let finnish = words.finnish.trim().toLowerCase();
@@ -39,6 +47,12 @@ function validatePost(words) {
   }
 }
 
+/**
+ * Returns an error message according to given parameter.
+ * 
+ * @param {string} error - Type of error.
+ * @returns Error message.
+ */
 function getErrorCode(error) {
   if (error === undefined) {
     return "";
@@ -51,6 +65,9 @@ function getErrorCode(error) {
   }
 }
 
+/**
+ * Routers a get-all-data-request to crudrepository and returns either the data or an error.
+ */
 router.get("/", async (req, res) => {
   try {
     let vocabulary = await connection.getAll();
@@ -60,6 +77,15 @@ router.get("/", async (req, res) => {
   }
 });
 
+/**
+ * Routers a post data to the crudrepository.
+ * Given data is validated before posting and if it fails, responses as an error.
+ * If an error occurs after data is posted, the error is checked if it is a 
+ * duplication error and response error is send according to that.
+ * 
+ * If post is a success, response is 
+ * {words: {english: validated english word, finnish: validated finnish word}, id: item id}
+ */
 router.post("/", async (req, res) => {
   let words = req.body;
   const result = validatePost(words);
@@ -84,6 +110,11 @@ router.post("/", async (req, res) => {
   }
 });
 
+/**
+ * Routers a delete request by id to crudrepository.
+ * If an error does not occur, the response is checked if an item has been deleted 
+ * or not and according to that either responses as an 'ok' or as error.
+ */
 router.delete("/:id([0-9]+)", async (req, res) => {
   const id = req.params.id;
   try {
@@ -98,6 +129,15 @@ router.delete("/:id([0-9]+)", async (req, res) => {
   }
 });
 
+/**
+ * Routers a put request to crudrepository.
+ * Given data is validated before sending and if it fails, responses as an error.
+ * If an error occurs after data is sent, the error is checked if it is a 
+ * duplication error and response error is send according to that.
+ * 
+ * If the update is a success, response is 
+ * {words: {english: validated english word, finnish: validated finnish word}, id: item id}
+ */
 router.put("/:id([0-9]+)", async (req, res) => {
   const id = req.params.id;
   const words = req.body;
